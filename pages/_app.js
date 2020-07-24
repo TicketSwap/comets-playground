69d3c552564cd05b7f818f92f75f5046124baba6
Glenn <glenngijsberts95@gmail.com>
2020-07-24 16:18:15 +0200

    Fix serverside issue emotion

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import NextApp from 'next/app'
+import { CacheProvider } from '@emotion/core'
+import { cache } from 'emotion'
+
+export default class App extends NextApp {
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <CacheProvider value={cache}>
+        <Component {...pageProps} />
+      </CacheProvider>
+    )
+  }
+}
