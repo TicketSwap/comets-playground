e2d3c9f5e65a9229211ab233851c25e42f84e0bb
Glenn <glenngijsberts95@gmail.com>
2020-07-24 16:18:23 +0200

    Implement color picker

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import Head from 'next/head'
 import {
   Cover,
@@ -15,7 +15,8 @@ import { MagnifyingGlass } from '@ticketswap/comets'
 import styled from '@emotion/styled'
 import { WidePageLayout, NarrowPageLayout } from '../components/PageLayout'
 import icons from '../icons'
-import Icon from '../components/Component'
+import Component from '../components/Component'
+import { ChromePicker as ColorPicker } from 'react-color'
 
 const StyledCover = styled(Cover)`
   text-align: center;
@@ -53,13 +54,14 @@ const IconGrid = styled.div`
 
 const IconWrapper = styled.div`
   cursor: pointer;
+  box-shadow: ${shadow.light};
 `
 
 const IconBlock = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 1px solid ${color.stardust};
+  border-bottom: 1px solid ${color.stardust};
   border-radius: 4px 4px 0px 0px;
   padding: ${space[48]} ${space[32]};
 `
@@ -67,7 +69,6 @@ const IconBlock = styled.div`
 const TextBlock = styled.div`
   text-align: center;
   background-color: ${color.nova};
-  box-shadow: ${shadow.light};
   padding: ${space[32]};
 `
 
@@ -78,21 +79,57 @@ const IconLabel = styled.p`
 `
 
 const EditorBar = styled.div`
-  /* background-color: ${color.stardust}; */
   width: 100%;
   padding: ${space[32]};
   border-bottom: 1px solid ${color.stardust};
 `
 
+const EditorGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 56px 56px;
+  grid-gap: 16px;
+`
+
+const ColorBlockWrapper = styled.div`
+  position: relative;
+  align-self: end;
+`
+
+const ColorBlock = styled.div`
+  border-radius: 4px;
+  width: 56px;
+  height: 56px;
+  cursor: pointer;
+`
+
 export default function Home() {
-  const [color, setColor] = useState('#fff')
+  const [color, setColor] = useState('#00b6f0')
   const [size, setSize] = useState(32)
-  const [backgroundColor, setBackgroundColor] = useState('#000')
   const [search, setSearch] = useState('')
+  const [showColorPicker, setShowColorPicker] = useState(false)
 
   const filteredIcons = Object.keys(icons).filter(icon =>
-    icon.toLowerCase().includes(search)
+    icon.toLowerCase().includes(search.toLowerCase())
   )
+
+  const colorPickerRef = useRef()
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleColorpickerClick)
+
+    return () => {
+      document.removeEventListener('mousedown', handleColorpickerClick)
+    }
+  }, [])
+
+  const handleColorpickerClick = e => {
+    if (colorPickerRef.current.contains(e.target)) {
+      setShowColorPicker(true)
+      return
+    }
+    // outside click
+    setShowColorPicker(false)
+  }
 
   return (
     <div className="container">
@@ -116,15 +153,55 @@ export default function Home() {
 
       <EditorBar>
         <NarrowPageLayout>
-          <Input
-            placeholder="Search for icons"
-            id="search"
-            label="Search"
-            hideLabel
-            value={search}
-            onChange={v => setSearch(v.target.value)}
-            leftAdornment={<MagnifyingGlass size={24} />}
-          />
+          <EditorGrid>
+            <Input
+              placeholder="Search for icons"
+              id="search"
+              label="Search"
+              // hideLabel
+              value={search}
+              onChange={v => setSearch(v.target.value)}
+              leftAdornment={<MagnifyingGlass size={24} />}
+            />
+
+            <ColorBlockWrapper ref={colorPickerRef}>
+              <ColorBlock
+                style={{ backgroundColor: color }}
+                onClick={() => setShowColorPicker(true)}
+              />
+
+              {showColorPicker && (
+                <div
+                  style={{
+                    position: 'absolute',
+                    zIndex: 9999,
+                    right: 0,
+                  }}
+                >
+                  <ColorPicker
+                    color={color}
+                    onChange={color => setColor(color.hex)}
+                  />
+                </div>
+              )}
+            </ColorBlockWrapper>
+
+            <Input
+              id="size"
+              label="Size"
+              value={size}
+              type="number"
+              min={0}
+              max={88}
+              onChange={e => {
+                if (e.target.value > 88) {
+                  return setSize(88)
+                }
+
+                setSize(e.target.value)
+              }}
+            />
+          </EditorGrid>
         </NarrowPageLayout>
       </EditorBar>
 
@@ -134,8 +211,8 @@ export default function Home() {
             {filteredIcons.map((icon, index) => {
               return (
                 <IconWrapper key={index}>
-                  <IconBlock style={{ backgroundColor }}>
-                    <Icon size={size} tag={icon} color={color} />
+                  <IconBlock>
+                    <Component size={size} tag={icon} color={color} />
                   </IconBlock>
                   <TextBlock>
                     <IconLabel>{icon}</IconLabel>
@@ -159,11 +236,6 @@ export default function Home() {
 
         * {
           box-sizing: border-box;
-        }
-
-        ::selection {
-          background-color: ${color.earth};
-          color: ${color.nova};
         }
       `}</style>
     </div>
